6c0cc2ccfeb44c4d5d90b88cb06882449b79de8e
SSH4240 <pdxucxy@gmail.com>
2023-07-04 22:37:22 +0900

    study

---
+++ src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.study.datajpa.repository;
+
+import com.study.datajpa.domain.Member;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Test
+    void 회원조회(){
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long save = memberRepository.save(member);
+        Member findMember = memberRepository.find(save);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
