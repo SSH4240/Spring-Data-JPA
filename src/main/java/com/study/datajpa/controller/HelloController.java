f1619e922ef3ff996bd376924282681a7d2b1991
SSH4240 <pdxucxy@gmail.com>
2023-07-29 18:21:34 +0900

    study

--- src/main/java/com/study/datajpa/controller/HelloController.java
+++ src/main/java/com/study/datajpa/controller/HelloController.java
@@ -10,6 +10,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
+        //  edit
         return "hello";
     }
 }
