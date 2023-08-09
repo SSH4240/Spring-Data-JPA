4cfbdab91e52338c2ada64c92a5affb5cb2bdf83
SSH4240 <pdxucxy@gmail.com>
2023-08-09 22:31:50 +0900

    study

--- src/main/java/com/study/datajpa/domain/Category.java
+++ src/main/java/com/study/datajpa/domain/Category.java
@@ -13,13 +13,14 @@ import java.util.List;
 @Setter
 public class Category {
     @Id @GeneratedValue
+    @Column(name = "category_id")
     private Long id;
     private String name;
 
     @ManyToMany
     @JoinTable(name = "category_item",
             joinColumns = @JoinColumn(name = "category_id"),
-    inverseJoinColumns = @JoinColumn(name = "item_id"))
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
