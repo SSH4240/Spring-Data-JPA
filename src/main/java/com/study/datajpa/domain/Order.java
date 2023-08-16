ccdea4c6dc2b17c5223ade056603972cf56a8fef
SSH4240 <pdxucxy@gmail.com>
2023-08-16 22:18:26 +0900

    study

--- src/main/java/com/study/datajpa/domain/Order.java
+++ src/main/java/com/study/datajpa/domain/Order.java
@@ -24,12 +24,22 @@ public class Order {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
     private OrderStatus status;
+
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
