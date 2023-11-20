23707daccace441afa4e2377a83ba1fa42147439
SSH4240 <pdxucxy@gmail.com>
2023-11-20 23:33:29 +0900

    study

--- src/main/java/com/study/datajpa/repository/MemberRepository.java
+++ src/main/java/com/study/datajpa/repository/MemberRepository.java
@@ -3,28 +3,34 @@ package com.study.datajpa.repository;
 import com.study.datajpa.domain.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
 
-    public Long save(Member member){
+    public Long save(Member member) {
         em.persist(member);
         return member.getId();
     }
 
-    public Member find(Long id){
+    public Member find(Long id) {
         return em.find(Member.class, id);
     }
-    public Member findOne(Long id){
+
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
 
     }
-    public Member findMembers(Long id){
-        return em.find(Member.class, id);
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class).setParameter("name", name).getResultList();
     }
 }
